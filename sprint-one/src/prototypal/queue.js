99dee24c1f319baf696950f06b0f7e5eac353538
Timothy <timnguy17@gmail.com>
2022-02-25 13:32:14 -0800

    pass tests for both prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  //returns a blank object that will link to queue methods--delegates failed object look ups to queuemethods
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.back = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.back] = value;
+  this.back++;
+};
+
+queueMethods.dequeue = function () {
+  var temp = this.storage[this.front];
+  delete this.storage[this.front];
+  this.front++;
+  return temp;
+};
+
+queueMethods.size = function () {
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.keys = 0;
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.keys] = value;
+  this.keys++;
+};
+
+stackMethods.pop = function() {
+  if (this.keys > 0) {
+    delete this.storage[this.keys];
+    this.keys--;
+    return this.storage[this.keys];
+  }
+};
+
+stackMethods.size = function() {
+  return this.keys;
+};
 
 
