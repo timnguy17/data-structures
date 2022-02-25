612fd9f3c227dc4871c1124727293bb8c2d28af5
Timothy <timnguy17@gmail.com>
2022-02-25 11:37:34 -0800

    pass tests for functionsal shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.keys = 0;
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.keys] = value;
+  this.keys++;
+};
+
+stackMethods.pop = function() {
+  if (this.keys > 0) {
+    delete this.storage[this.keys];
+    this.keys--;
+    return this.storage[this.keys];
+  }
+};
+
+stackMethods.size = function() {
+  return this.keys;
+};
 
 
