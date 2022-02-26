d15ac0e2a39ef31f12b40b0d7f0c44411fb8727d
Robert Taylor <rvtaylor94@gmail.com>
2022-02-25 17:07:00 -0700

    passed all the tests for sprint-two/linkedList.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+
+};
+var someInstance = new Stack();
+
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
-
+console.log(someInstance);
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,29 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let tailNode = new Node(value); // {'value': 4, 'next': null}
+    list.tail = tailNode;
+    if (list.head === null) {
+      list.head = tailNode;
+    }
   };
 
   list.removeHead = function() {
+    let deletedVal = list.head.value;
+    list.head.value = list.tail.value;
+    list.tail.value = null;
+    return deletedVal;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
+  console.log(list);
   return list;
 };
 
@@ -21,7 +36,7 @@ var Node = function(value) {
   node.value = value;
   node.next = null;
 
-  return node;
+  return node; // {'value': 4, 'next': null}
 };
 
 /*
